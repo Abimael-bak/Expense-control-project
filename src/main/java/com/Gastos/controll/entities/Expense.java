e2d40ce285e032a41ee0ecf5830afb01314712b0
Abimael-bak <abimaelabreu88@gmail.com>
2026-01-08 11:37:19 -0300

    :sparkles: feat(class): creating the Expense class

---
+++ src/main/java/com/Gastos/controll/entities/Expense.java
@@ -0,0 +1,72 @@
+package com.Gastos.controll.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_Expense")
+public class Expense implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	private Double amount;
+	private Date date;
+	
+	public Expense() {
+		
+	}
+
+	public Expense(Long id, String description, Double amount, Date date) {
+		this.id = id;
+		this.description = description;
+		this.amount = amount;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	
+	
+}
