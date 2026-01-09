26ce3f6b974a9546799686b2ad27a2c03eebb7ed
Abimael-bak <abimaelabreu88@gmail.com>
2026-01-09 19:20:34 -0300

    :wrench: :sparkles: Adding the properties for H2 and creating the ConfigTest test class

---
+++ src/main/java/com/Gastos/controll/testConfig/ConfigTest.java
@@ -0,0 +1,18 @@
+package com.Gastos.controll.testConfig;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class ConfigTest implements CommandLineRunner  {
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		
+	}
+
+}
