d9c19beacbf57c12ac8e6dd9a61062cdc2ea7d05
Abimael-bak <abimaelabreu88@gmail.com>
2026-01-09 11:05:03 -0300

    :sparkles: Adding the findById method in Resource

--- src/main/java/com/Gastos/controll/resource/ExpenseResource.java
+++ src/main/java/com/Gastos/controll/resource/ExpenseResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,11 @@ public class ExpenseResource {
 	public ResponseEntity<List<Expense>> findAll(){
 		List<Expense> expenses = expenseService.findAll();
 		return ResponseEntity.ok().body(expenses);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Expense> findById(@PathVariable Long id){
+		Expense expense = expenseService.findById(id);
+		return ResponseEntity.ok().body(expense);
 	}
 }
