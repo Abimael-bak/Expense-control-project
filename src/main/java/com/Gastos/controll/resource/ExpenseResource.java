8003bf7c08431ebba4f771951872e835598e235d
Abimael-bak <abimaelabreu88@gmail.com>
2026-01-09 11:23:27 -0300

    :sparkles: Adding the insert method

--- src/main/java/com/Gastos/controll/resource/ExpenseResource.java
+++ src/main/java/com/Gastos/controll/resource/ExpenseResource.java
@@ -1,13 +1,17 @@
 package com.Gastos.controll.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.Gastos.controll.entities.Expense;
 import com.Gastos.controll.service.ExpenseService;
@@ -30,5 +34,13 @@ public class ExpenseResource {
 	public ResponseEntity<Expense> findById(@PathVariable Long id){
 		Expense expense = expenseService.findById(id);
 		return ResponseEntity.ok().body(expense);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Expense> insert(@RequestBody Expense obj){
+		 Expense expense = expenseService.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		 return ResponseEntity.created(uri).body(expense);
+		
 	}
 }
