7d4deca5cdb816fb531681a851649f912c92fe71
Abimael-bak <abimaelabreu88@gmail.com>
2026-01-31 15:22:05 -0300

    :sparkles: Adding the loadExpenses and renderExpense functions to script.js

--- frontend/script.js
+++ frontend/script.js
@@ -11,3 +11,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
+function loadExpenses(){
+   fetch(API_URL)
+   .then(response => response.json())
+   .then(data => renderExpense(data))
+   .catch(error => console.error("Error fetching expenses:", error));
+}
+
+function renderExpense(expenses){
+    const tableBody = document.getElementById("expense-list")
+    const totalSpan = document.getElementById("total");
+
+    tableBody.innerHTML = "";
+    let total = 0;
+
+    expenses.forEach(e => {
+         
+        const date = e.moment.split("T")[0];
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td>${e.description}</td>
+            <td>${e.amount.toFixed(2)}</td>
+            <td>${e.category.name}</td>
+            <td>${date}</td>
+            <td>
+                <button onclick="Update(${e.id})">Atualizar</button>
+                <button onclick="Delete(${e.id})">Deletar</button>
+            </td>
+        `;
+        tableBody.appendChild(tr);
+        total += e.amount;
+        
+    });
+
+    totalSpan.textContent = total.toFixed(2);
+}
