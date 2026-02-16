4e0adcc575e2a19224b022d57d41e9218b0f4a07
Abimael-bak <abimaelabreu88@gmail.com>
2026-02-16 10:42:34 -0300

    :sparkles: adding the registration and sanitize methods

--- frontend/login.js
+++ frontend/login.js
@@ -36,4 +36,48 @@ function login() {
     });
 }
 
+function cadastro() {
+    const inputName = sanitize(document.getElementById("name").value.trim());
+    const inputEmail = sanitize(document.getElementById("Email").value.trim());
+    const inputPassword = sanitize(document.getElementById("password").value.trim());
+
+    if (!inputName || !inputEmail || !inputPassword) {
+        alert("Preencha todos os campos");
+        return;
+    }
+
+    const user = {
+        name: inputName,
+        email: inputEmail,
+        password: inputPassword
+    }
+
+    fetch(`${API_URL}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Erro ao cadastrar usuário");
+        }
+        return response.json();
+    })
+    .then(user => {
+        window.location.href = "login.html";
+    })
+    .catch(error => {
+        console.error("Erro ao cadastrar usuário:", error);
+        alert("Erro ao cadastrar usuário. Tente novamente.");
+    });
+}
+
+
+function sanitize(input) {
+    const div = document.createElement("div");
+    div.textContent = input;
+    return div.innerHTML;
+}
 
